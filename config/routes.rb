1130fa171462af56210eefcb5cff0bc288f29435
Morgane Bourdillon <mlacroix8@gmail.com>
2024-11-15 16:22:30 +0100

    end exercice

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: [:create, :destroy]
 end
