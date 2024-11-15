07ad596e12de991d90b8ae38034496c63a7a7a18
Morgane Bourdillon <mlacroix8@gmail.com>
2024-11-15 15:24:58 +0100

    Add new img

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require "json"
-require "open-uri"
+require 'json'
+require 'open-uri'
 
 puts 'Creating movies...'
 
